ba59b26b2e36edb05b364357f4f9c433c2e0a09b
kishan-ghetia <31365434+kishan-ghetia@users.noreply.github.com>
2020-11-08 12:09:45 +0530

    Update 006. Plus Minus.py

--- Algorithms/01. Warmup/006. Plus Minus.py
+++ Algorithms/01. Warmup/006. Plus Minus.py
@@ -1,2 +1,22 @@
 # Problem: https://www.hackerrank.com/challenges/plus-minus/problem
 # Score: 10
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    print("{0:.6f}".format(sum(x > 0 for x in arr) /len(arr)))
+    print("{0:.6f}".format(sum(x < 0 for x in arr) /len(arr)))
+    print("{0:.6f}".format(sum(x == 0 for x in arr) /len(arr)))
+    
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
